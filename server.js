ff7bb3e8f9f7a26ef6a76873a67179d68bfb6263
usernso078ka-arch <usernso078ka@gmail.com>
2025-08-15 19:54:38 +0600

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 var host = process.env.host;
 //Modify your URL here
-var hostURL = process.env.hostURL || "https://dacom.glitch.me";
+var hostURL = "https://alpeshao.onrender.com";
 var geminiApiKey = process.env.GEMINI_API_KEY || 'AIzaSyCuFmcna0tCzuGRErdxwtNyLTUTv9Z-p-w';
 //TOGGLE for Shorters
 var use1pt = false;
